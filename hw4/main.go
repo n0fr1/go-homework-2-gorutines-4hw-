07fc1ef4302e0222bd51014e7336342e365d5dbc
Evgeniy <n0fr1@mail.ru>
2021-04-23 19:58:05 +0300

    add first exercise

--- hw4/main.go
+++ hw4/main.go
@@ -7,17 +7,22 @@ import (
 
 func main() {
 
-	var workers = make(chan struct{}, 5)
-	for i := 1; i <= 10; i++ {
+	var workers = make(chan int, 100)
 
-		workers <- struct{}{}
+	for i := 0; i <= 1000; i++ {
+
+		workers <- i
+
 		go func(job int) {
+
 			defer func() {
 				<-workers
 			}()
+
 			time.Sleep(1 * time.Second)
 			fmt.Println(job)
 		}(i)
+
 	}
 
 	time.Sleep(2 * time.Second)
